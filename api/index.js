d94ebed61e015603b3a221f5cd4096d06ead991d
theabhipatel <theabhipatel.co@gmail.com>
2026-09-13 14:26:52 +0530

    fix: vercel configuration issue

--- api/index.js
+++ api/index.js
@@ -4,6 +4,6 @@
  * Vercel deploy entry handler for serverless deployment.
  * This wraps the Express app as a Vercel serverless function.
  */
-const app = require("../frontend/dist/app.js").default;
+const app = require("../backend/dist/app.js").default;
 
 module.exports = (req, res) => app(req, res);
